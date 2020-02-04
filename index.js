4d84ab0dba3012651a075a79d29b614672e17ea6
bertearazvan <bertea74@gmail.com>
2020-02-04 09:43:26 +0100

    asdas

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const express = require("express");
 
 var app = express();
 
-app.use(express.static("public"));
+const path = require("path");
+const port = 3000;
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
