7d8adac45148a7ba76efcdd5f5c4f45c7c357633
bertearazvan <bertea74@gmail.com>
2020-02-04 10:31:09 +0100

    asd

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const express = require("express");
 var app = express();
 
 const path = require("path");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
+if (process.env.NODE_ENV === "production") {
+    //Set static folder
+    app.use(express.static("/"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "index.html"));
+    });
+}
+
+app.listen((port), () => console.log(`url-shortener listening on port ${port}!`));
